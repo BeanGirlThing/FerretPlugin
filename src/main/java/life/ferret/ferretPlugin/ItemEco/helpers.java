d924c46c350497bc36e06837181204141bd6ca21
BeanGirlThing <jessica.ampstead@outlook.com>
2022-12-07 04:39:48 +0000

    Update helpers.java to use apache lang3

--- src/main/java/life/ferret/ferretPlugin/ItemEco/helpers.java
+++ src/main/java/life/ferret/ferretPlugin/ItemEco/helpers.java
@@ -1,6 +1,6 @@
 package life.ferret.ferretPlugin.ItemEco;
 
-import org.apache.commons.lang.math.NumberUtils;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.bukkit.Material;
 
 public class helpers {
